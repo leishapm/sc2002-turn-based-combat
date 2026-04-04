e3de4c097a6708b5e97b94e70e9abb334fb77082
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-04 15:36:49 +0800

    Add SkillsResult class with damage, buff, and effects handling

---
+++ src/main/java/combatarena/util/SkillsResult.java
@@ -0,0 +1,48 @@
+package combatarena.util;
+
+import combatarena.effects.Effects;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SkillsResult {
+
+    private int damageGiven;
+    private int buffReceived;
+    private List<Effects> effectsDone;
+
+    public SkillsResult() {
+        this.damageGiven = 0;
+        this.buffReceived = 0;
+        this.effectsDone = new ArrayList<>();
+    }
+
+    public SkillsResult(int damageGiven) {
+        this.damageGiven = damageGiven;
+        this.buffReceived = 0;
+        this.effectsDone = new ArrayList<>();
+    }
+
+    public SkillsResult(int damageGiven, int buffReceived) {
+        this.damageGiven = damageGiven;
+        this.buffReceived = buffReceived;
+        this.effectsDone = new ArrayList<>();
+    }
+
+    // add effect to result
+    public void addEffect(Effects effect) {
+        effectsDone.add(effect);
+    }
+
+    public int getDamageGiven() {
+        return damageGiven;
+    }
+
+    public int getBuffReceived() {
+        return buffReceived;
+    }
+
+    public List<Effects> getEffectsDone() {
+        return effectsDone;
+    }
+}
