8a4eeefe05a6cea941e7edab11cf76904ce95f9e
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:37:54 +0800

    Update Level.java

--- src/main/java/combatarena/level/Level.java
+++ src/main/java/combatarena/level/Level.java
@@ -1,24 +1,20 @@
 package combatarena.level;
 
-import combatarena.entities.Character;
 import combatarena.entities.Enemy;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Level {
 
     protected int levelNumber;
     protected String difficulty;
-    protected List<Character> startingEnemyPool;
-    protected List<Character> backupSpawn;
+    protected List<Enemy> startingEnemyPool;
+    protected List<Enemy> backupSpawn;
+    protected boolean backupSpawnTriggered;
 
-    public Level(int levelNumber, String difficulty) {
-        this.levelNumber = levelNumber;
-        this.difficulty = difficulty;
-        this.startingEnemyPool = new ArrayList<>();
-        this.backupSpawn = new ArrayList<>();
-    }
+    public abstract List<Enemy> getInitialWave();
+
+    public abstract List<Enemy> getBackupWave();
 
     public int getLevelNumber() {
         return levelNumber;
@@ -26,22 +22,5 @@ public abstract class Level {
 
     public String getDifficulty() {
         return difficulty;
-    }
-
-    public List<Character> getStartingEnemyPool() {
-        return startingEnemyPool;
-    }
-
-    // returns only enemies from backupSpawn
-    public List<Enemy> getBackupWave() {
-        List<Enemy> enemies = new ArrayList<>();
-
-        for (Character c : backupSpawn) {
-            if (c instanceof Enemy) {
-                enemies.add((Enemy) c);
-            }
-        }
-
-        return enemies;
     }
 }
