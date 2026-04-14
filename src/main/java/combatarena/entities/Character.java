d4b24966c8c4baed0b0a089cb5ffc00c9e31eed7
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:41:12 +0800

    Update Character.java

--- src/main/java/combatarena/entities/Character.java
+++ src/main/java/combatarena/entities/Character.java
@@ -3,72 +3,91 @@ package combatarena.entities;
 import combatarena.effects.StatusEffect;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public abstract class Character {
 
     protected int hp;
-    protected int maxHp;
-
-    protected int baseAttack;
-    protected int baseDefense;
+    protected int attack;
+    protected int defense;
     protected int speed;
+    protected int maxHp;
+    protected List<StatusEffect> activeEffects;
 
-    protected List<StatusEffect> activeEffects = new ArrayList<>();
+    public Character(int hp, int attack, int defense, int speed) {
+        this.hp = hp;
+        this.maxHp = hp;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.activeEffects = new ArrayList<>();
+    }
+
+    public void updateEffects() {
+
+        Iterator<StatusEffect> iterator = activeEffects.iterator();
+
+        while (iterator.hasNext()) {
+            StatusEffect effect = iterator.next();
+
+            effect.apply(this);
+            effect.decrementDuration();
+
+            if (effect.isExpired()) {
+                iterator.remove();
+            }
+        }
+    }
+
+    public void addEffect(StatusEffect effect) {
+        activeEffects.add(effect);
+    }
+
+    public void removeEffect(StatusEffect effect) {
+        activeEffects.remove(effect);
+    }
 
     public boolean isAlive() {
         return hp > 0;
     }
 
-    public int getAttack() {
-        int bonus = 0;
-        for (StatusEffect e : activeEffects) {
-            bonus += e.getAttackBonus();
-        }
-        return baseAttack + bonus;
-    }
-
-    public int getDefense() {
-        int bonus = 0;
-        for (StatusEffect e : activeEffects) {
-            bonus += e.getDefenseBonus();
-        }
-        return baseDefense + bonus;
-    }
-
     public void takeDamage(int dmg) {
-        hp -= dmg;
-        if (hp < 0) hp = 0;
+
+        int reducedDamage = Math.max(0, dmg - defense);
+        hp -= reducedDamage;
+
+        if (hp < 0) {
+            hp = 0;
+        }
     }
 
     public void heal(int amount) {
+
         hp += amount;
-        if (hp > maxHp) hp = maxHp;
-    }
 
-    public void addEffect(StatusEffect effect) {
-        effect.apply(this);
-        activeEffects.add(effect);
-    }
-
-    public void applyEffects() {
-        List<StatusEffect> toRemove = new ArrayList<>();
-
-        for (StatusEffect e : activeEffects) {
-            e.tick(this);
-            if (e.isExpired()) {
-                e.remove(this);
-                toRemove.add(e);
-            }
+        if (hp > maxHp) {
+            hp = maxHp;
         }
-
-        activeEffects.removeAll(toRemove);
     }
 
-    public boolean isStunned() {
-        for (StatusEffect e : activeEffects) {
-            if (e.isStun()) return true;
-        }
-        return false;
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public List<StatusEffect> getActiveEffects() {
+        return activeEffects;
     }
 }
