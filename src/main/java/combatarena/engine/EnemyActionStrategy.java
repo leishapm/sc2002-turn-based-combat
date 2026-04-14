3cf77f3e9115d44900738ecb4fc6b53182d13a57
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 12:52:00 +0800

    Update EnemyActionStrategy.java

--- src/main/java/combatarena/engine/EnemyActionStrategy.java
+++ src/main/java/combatarena/engine/EnemyActionStrategy.java
@@ -4,9 +4,6 @@ import combatarena.entities.Enemy;
 import combatarena.entities.Character;
 import combatarena.actions.Action;
 
-import java.util.List;
-
 public interface EnemyActionStrategy {
-
-    Actions chooseAction(Enemy enemy, List<Character> targets);
+    Action chooseAction(Enemy enemy, Character target);
 }
