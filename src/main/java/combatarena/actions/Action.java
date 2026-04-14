1c89ca76ccba466bea07e74813e0a9f422422784
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 12:01:39 +0800

    Rename Actions class to Action

--- src/main/java/combatarena/actions/Action.java
+++ src/main/java/combatarena/actions/Action.java
@@ -3,16 +3,16 @@ package combatarena.actions;
 import combatarena.entities.Character;
 import combatarena.util.SkillsResult;
 
-public abstract class Actions {
+public abstract class Action {
 
     protected String name;
 
-    public Actions(String name) {
+    public Action(String name) {
         this.name = name;
     }
 
     // executes the action and returns the result (damage, effects, etc.)
-    public abstract SkillsResult execute(Character user, Character target);
+    public abstract ActionResult execute(Character user, Character target);
 
     // returns description of action (for UI/logging)
     public abstract String info();
