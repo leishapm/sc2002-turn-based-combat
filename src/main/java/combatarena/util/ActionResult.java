fb21f36ebb6c650c902198ccbacefb99561ffc46
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:28:47 +0800

    Update ActionResult.java

--- src/main/java/combatarena/util/ActionResult.java
+++ src/main/java/combatarena/util/ActionResult.java
@@ -14,19 +14,8 @@ public class ActionResult {
     private List<StatusEffect> effectsApplied;
     private List<Character> defeatedTargets;
 
-    public ActionResult(int damage, int heal) {
-        this.damageGiven = damage;
-        this.healAmount = heal;
-        this.buffAmount = 0;
+    public ActionResult() {
         this.effectsApplied = new ArrayList<>();
-        this.defeatedTargets = new ArrayList<>();
-    }
-
-    public ActionResult(int damage, int heal, List<StatusEffect> effects) {
-        this.damageGiven = damage;
-        this.healAmount = heal;
-        this.buffAmount = 0;
-        this.effectsApplied = (effects != null) ? effects : new ArrayList<>();
         this.defeatedTargets = new ArrayList<>();
     }
 
@@ -34,19 +23,39 @@ public class ActionResult {
         return damageGiven;
     }
 
+    public void setDamageGiven(int damageGiven) {
+        this.damageGiven = damageGiven;
+    }
+
     public int getHealAmount() {
         return healAmount;
+    }
+
+    public void setHealAmount(int healAmount) {
+        this.healAmount = healAmount;
     }
 
     public int getBuffAmount() {
         return buffAmount;
     }
 
+    public void setBuffAmount(int buffAmount) {
+        this.buffAmount = buffAmount;
+    }
+
     public List<StatusEffect> getEffectsApplied() {
         return effectsApplied;
     }
 
+    public void addEffect(StatusEffect effect) {
+        this.effectsApplied.add(effect);
+    }
+
     public List<Character> getDefeatedTargets() {
         return defeatedTargets;
+    }
+
+    public void addDefeatedTarget(Character target) {
+        this.defeatedTargets.add(target);
     }
 }
