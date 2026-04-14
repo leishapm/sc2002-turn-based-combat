16c201cc189c876532807f059c88ce9fa8c06347
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:57:57 +0800

    Update StatusEffect.java

--- src/main/java/combatarena/effects/StatusEffect.java
+++ src/main/java/combatarena/effects/StatusEffect.java
@@ -4,20 +4,31 @@ import combatarena.entities.Character;
 
 public abstract class StatusEffect {
 
-    protected int duration;
     protected String typeOfEffect;
+    protected int duration;
+
+    public StatusEffect(String typeOfEffect, int duration) {
+        this.typeOfEffect = typeOfEffect;
+        this.duration = duration;
+    }
 
     public abstract void apply(Character target);
+
     public abstract void tick(Character target);
+
     public abstract void remove(Character target);
+
+    public void decrementDuration() {
+        duration--;
+    }
 
     public boolean isExpired() {
         return duration <= 0;
     }
 
-    public int getAttackBonus() { return 0; }
-    public int getDefenseBonus() { return 0; }
-    public boolean isStun() { return false; }
+    public int getDuration() {
+        return duration;
+    }
 
     public String getTypeOfEffect() {
         return typeOfEffect;
