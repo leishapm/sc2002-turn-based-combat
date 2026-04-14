ed7ae7d56b883614a22e6d3295c25e7e9c05a67f
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:44:45 +0800

    Update Action.java

--- src/main/java/combatarena/actions/Action.java
+++ src/main/java/combatarena/actions/Action.java
@@ -4,20 +4,6 @@ import combatarena.engine.ActionContext;
 import combatarena.util.ActionResult;
 
 public abstract class Action {
-
-    protected String name;
-
-    public Action(String name) {
-        this.name = name;
-    }
-
     public abstract ActionResult execute(ActionContext context);
-
-    public String info() {
-        return name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    public abstract String info();
 }
