2a56d72269c57d40753efeeb2c8af53f72766348
Leisha Menezes <leishapr001@e.ntu.edu.sg>
2026-04-14 19:57:26 +0800

    Create SpecialSkill abstract class for skills

---
+++ src/main/java/combatarena/actions/SpecialSkill.java
@@ -0,0 +1,9 @@
+package combatarena.skills;
+
+import combatarena.engine.ActionContext;
+import combatarena.util.ActionResult;
+
+public abstract class SpecialSkill {
+    public abstract ActionResult execute(ActionContext context);
+    public abstract ActionResult executeEffect(ActionContext context);
+}
