f896e5a0da7541bc2de7307da643431f931a1e5f
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 14:08:45 +0800

    Update ActionResult.java

--- src/main/java/combatarena/util/ActionResult.java
+++ src/main/java/combatarena/util/ActionResult.java
@@ -9,18 +9,24 @@ public class ActionResult {
 
     private int damageGiven;
     private int healAmount;
+    private int buffAmount;
     private List<StatusEffect> effectsApplied;
+    private List<Character> defeatedTargets;
 
     public ActionResult(int damage, int heal) {
         this.damageGiven = damage;
         this.healAmount = heal;
+        this.buffAmount = 0;
         this.effectsApplied = new ArrayList<>();
+        this.defeatedTargets = new ArrayList<>();
     }
 
     public ActionResult(int damage, int heal, List<StatusEffect> effects) {
         this.damageGiven = damage;
         this.healAmount = heal;
+        this.buffAmount = 0;
         this.effectsApplied = (effects != null) ? effects : new ArrayList<>();
+        this.defeatedTargets = new ArrayList<>();
     }
 
     public int getDamageGiven() {
@@ -31,7 +37,15 @@ public class ActionResult {
         return healAmount;
     }
 
+    public int getBuffAmount() {
+        return buffAmount;
+    }
+
     public List<StatusEffect> getEffectsApplied() {
         return effectsApplied;
+    }
+
+    public List<Character> getDefeatedTargets() {
+        return defeatedTargets;
     }
 }
