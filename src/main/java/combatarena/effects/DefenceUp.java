c37d9f44ed53f06b8e7d74cc8e44364270b9934d
Leisha Menezes <leisha.p.menezes@gmail.com>
2026-04-14 15:01:06 +0800

    Update DefenceUp.java

--- src/main/java/combatarena/effects/DefenceUp.java
+++ src/main/java/combatarena/effects/DefenceUp.java
@@ -1,18 +1,34 @@
 package combatarena.effects;
+
 import combatarena.entities.Character;
+
 public class DefenceUp extends StatusEffect {
+
     private int bonus = 10;
+    private boolean applied = false;
+
     public DefenceUp() {
         super("DefenceUp", 2);
     }
+
     @Override
-    public void apply(Character target) {}
+    public void apply(Character target) {
+        if (!applied) {
+            target.increaseDefense(bonus);
+            applied = true;
+        }
+    }
+
     @Override
     public void tick(Character target) {
         decrementDuration();
     }
+
     @Override
-    public void remove(Character target) {}
+    public void remove(Character target) {
+        target.decreaseDefense(bonus);
+    }
+
     public int getDefenceBonus() {
         return bonus;
     }
