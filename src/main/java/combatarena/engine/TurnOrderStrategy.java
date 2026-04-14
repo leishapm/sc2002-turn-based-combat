c2f4e8d0c642dc467501fd521f42f46ac143eacb
Leisha Menezes <leishapr001@e.ntu.edu.sg>
2026-04-14 20:01:32 +0800

    Remove unnecessary whitespace and comments

--- src/main/java/combatarena/engine/TurnOrderStrategy.java
+++ src/main/java/combatarena/engine/TurnOrderStrategy.java
@@ -5,7 +5,5 @@ import combatarena.entities.Character;
 import java.util.List;
 
 public interface TurnOrderStrategy {
-
-    // determines the order of turns based on some logic (e.g. speed)
     List<Character> determineTurnOrder(List<Character> combatants);
 }
