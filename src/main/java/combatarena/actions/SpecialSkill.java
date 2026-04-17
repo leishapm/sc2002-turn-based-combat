d3fb7530ae425a36a4e53d014bb096a2fd88970c
Leisha Menezes <leishapr001@e.ntu.edu.sg>
2026-04-17 21:48:36 +0800

    Extend SpecialSkill class to include info method

--- src/main/java/combatarena/actions/SpecialSkill.java
+++ src/main/java/combatarena/actions/SpecialSkill.java
@@ -3,7 +3,16 @@ package combatarena.actions;
 import combatarena.engine.ActionContext;
 import combatarena.util.ActionResult;
 
-public abstract class SpecialSkill {
+
+public abstract class SpecialSkill extends Action {
+
+    @Override
     public abstract ActionResult execute(ActionContext context);
+
     public abstract ActionResult executeEffect(ActionContext context);
+
+    @Override
+    public String info() {
+        return "Special Skill";
+    }
 }
